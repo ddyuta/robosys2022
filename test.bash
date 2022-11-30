ff2fb7ff6d2cb8b03c07162ade80a4f931ba09bd
Yuta_Domen <yutamarodd0730@gmail.com>
2022-11-30 15:45:41 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -1,3 +1,18 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Yuta Domen
+# SPDX-Licence-Identifier: BSD-3-Clause
 
-seq 5 |./plus
+ng (){
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+
+out=$(seq 5 |./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
+
